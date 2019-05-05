8496442ed41701d5f899b24077fc101e5f251377
EthChil <ethanchilderhose@gmail.com>
2019-05-05 00:39:51 -0400

    lmao

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
@@ -6,7 +7,12 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+import static org.opencv.imgcodecs.Imgcodecs.imdecode;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
 
 public class Main {
 
@@ -32,6 +38,9 @@ public class Main {
 
         //System.load("/usr/local/Cellar/opencv/4.1.0_1/share/java/libopencv_java410.dylib");
 
+        //VARS
+        double maxAR = 1;
+
 
         JFrame frame = new JFrame("output");
         JFrame feed = new JFrame("feed");
@@ -42,31 +51,58 @@ public class Main {
         frame.setSize(700, 500);
         frame.add(image);
 
-        VideoCapture cam = new VideoCapture(0);
-
+        //VideoCapture cam = new VideoCapture();
         //cam.open(0);
 
         Mat src = new Mat();
+        src = Imgcodecs.imread("screw.jpg");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int VSub, VAdd, SSub, SAdd, HSub, HAdd;
+
+        VSub = 50;
+        VAdd = 255;
+        SSub = 75;
+        SAdd = 200;
+        HSub = 0;
+        HAdd = 255;
+
+        String text = "";
 
         //loops forever
         while (true) {
 
+            if(reader.ready()) {
+                text = reader.readLine();
+            }
+
+            String[] splited = text.split("\\s+");
+
+
+            if (splited.length >= 2){
+                VSub = Integer.parseInt(splited[0]);
+                VAdd = Integer.parseInt(splited[1]);
+
+                //SSub = Integer.parseInt(splited[0]);
+                //SAdd = Integer.parseInt(splited[1]);
+
+                //HSub = Integer.parseInt(splited[0]);
+                //HAdd = Integer.parseInt(splited[1]);
+            }
+
             //read a frame from the video in
-            cam.read(src);
+            //cam.read(src);
+
+            src = Imgcodecs.imread("screw.jpg");
+
 
             //Mats needed for image processing
             Mat dst = new Mat();
             Mat out = new Mat();
 
             //Image filter values
-            int VSub = 100;
-            int VAdd = 255;
 
-            int SSub = 100;
-            int SAdd = 255;
-
-            int HSub = 100;
-            int HAdd = 255;
 
             //resize the image to decrease the strain on the CPU
             Imgproc.resize(src, src, new Size(src.cols() / 2, src.rows() / 2));
@@ -89,11 +125,31 @@ public class Main {
             Mat hierarchy = new Mat();
 
             //create contours around shapes in the mask
-            Imgproc.findContours(dst, contours1, hierarchy1, 2, 1);
-            Imgproc.findContours(dst, contours2, hierarchy2, 2, 1);
+            Imgproc.findContours(dst, contours1, hierarchy1, 2, 1, new Point(0, 0));
+            Imgproc.findContours(dst, contours2, hierarchy2, 2, 1, new Point(0, 0));
 
             //copy the original image to the output Mat that the contours are overlayed to
             src.copyTo(out);
+
+            //Sort contours based on aspect ratio
+//            for (int i = 0; i < contours2.size(); i++) {
+//                    double sizeX = Imgproc.minAreaRect(new Mat(contours2.get(i))).size.width;
+//                    double sizeY = Imgproc.minAreaRect(new Mat(contours2.get(i))).size.height;
+//                    double ARy = sizeX / sizeY;
+//                    double ARx = sizeY / sizeX;
+//
+//
+//                    double AR = (ARy > ARx) ? ARy : ARx;
+//
+//                    if (AR > maxAR) {
+//                        contours2.remove(i);
+//                    }
+//            }
+
+            for (int i = 0; i < contours2.size(); i++) {
+                Imgproc.drawContours(out, contours2, i, new Scalar(255, 0, 255), 2, 8, hierarchy, 0, new Point());
+            }
+
 
 //            //Sort the contours by rough shape removing those that fall under a certain threshold
 //            for (int i = 0; i < contours2.size(); i++) {
@@ -118,40 +174,29 @@ public class Main {
 //
 //            }
 
-            //Sort contours based on aspect ratio
 
-            for (int i = 0; i < contours2.size(); i++) {
-                double sizeX = Imgproc.boundingRect(contours2.get(i)).width;
-                double sizeY = Imgproc.boundingRect(contours2.get(i)).width;
 
-                if (size > largest) {
-                    index = i;
-                    largest = size;
-                }
-
-            }
-
-            //draw the contour onto the output image
-            Imgproc.drawContours(out, contours2, index, new Scalar(255, 0, 255), 2, 8, hierarchy, 0, new Point());
-
-            //Checks to ensure there is a contour
-            if (index > -1) {
-
-                //variables for the different dimensions for the box
-                int x = Imgproc.boundingRect(contours2.get(index)).x;
-                int y = Imgproc.boundingRect(contours2.get(index)).y;
-
-                int width = Imgproc.boundingRect(contours2.get(index)).width;
-                int height = Imgproc.boundingRect(contours2.get(index)).height;
-
-                //prints the NavX turn angle on the RAS Pi this goes through the GPIO pins
-                System.out.println("angle to turn = " + (0.09375) * ((x + (width / 2)) - (out.cols() / 2)));
-
-                //places a rectangle over the output image
-                Imgproc.rectangle(out, new Point(x, y), new Point(x + width, y + height), new Scalar(255, 128, 0), 2, 2,
-                        0);
-
-            }
+//            //draw the contour onto the output image
+//            Imgproc.drawContours(out, contours2, index, new Scalar(255, 0, 255), 2, 8, hierarchy, 0, new Point());
+//
+//            //Checks to ensure there is a contour
+//            if (index > -1) {
+//
+//                //variables for the different dimensions for the box
+//                int x = Imgproc.minAreaRect(contours2.get(index)).x;
+//                int y = Imgproc.minAreaRect(contours2.get(index)).y;
+//
+//                int width = Imgproc.minAreaRect(contours2.get(index)).width;
+//                int height = Imgproc.minAreaRect(contours2.get(index)).height;
+//
+//                //prints the NavX turn angle on the RAS Pi this goes through the GPIO pins
+//                System.out.println("angle to turn = " + (0.09375) * ((x + (width / 2)) - (out.cols() / 2)));
+//
+//                //places a rectangle over the output image
+//                Imgproc.rectangle(out, new Point(x, y), new Point(x + width, y + height), new Scalar(255, 128, 0), 2, 2,
+//                        0);
+//
+//            }
 
             display(out);
 
